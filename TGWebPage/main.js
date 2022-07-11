081bfc75baaa52bd18d5acc151634bb6e0ffb2b0
Tyler Gurth <90432221+tgurth@users.noreply.github.com>
2022-07-11 18:16:32 -0400

    Uploaded.

---
+++ TGWebPage/main.js
@@ -0,0 +1,22 @@
+window.addEventListener( 'resize', sizePictures );
+
+/**
+ * Sizing and resizing the images that hold the social medias.
+ */
+let mediaPictures = document.getElementsByClassName('mediaPic');
+let projectPictures = document.getElementsByClassName('projectPic');
+sizePictures();
+
+function sizePictures() {
+    for (let i = 0; i < mediaPictures.length; i++) {
+        let w = (window.innerWidth / 6) + 'px';
+        mediaPictures[i].style.width = w;    
+        mediaPictures[i].style.height = 'auto';
+    }
+
+    for (let i = 0; i < projectPictures.length; i++) {
+        let w = (window.innerWidth / 9) + 'px';
+        projectPictures[i].style.width = w;    
+        projectPictures[i].style.height = 'auto';
+    }
+}
