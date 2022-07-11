a60df48d31c2c58b85b7a03b5db7ffe1afaeeba6
Tyler Gurth <tyler_gurth@brown.edu>
2022-07-11 18:48:29 -0400

    fixing sizing?

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 window.addEventListener( 'resize', sizePictures );
 
-if (window.innerWidth < 400) {
-    document.getElementById('pageTop').style.flexDirection = 'column';
-    document.getElementById('tyson').style.scale = '0.5';
+size(600)
+
+function size(min) {
+    if (window.innerWidth < min) {
+        document.getElementById('pageTop').style.flexDirection = 'column';
+        document.getElementById('tyson').style.scale = '0.5';
+    }
 }
+
 
 /**
  * Sizing and resizing the images that hold the social medias.
@@ -24,9 +29,5 @@ function sizePictures() {
         projectPictures[i].style.width = w;    
         projectPictures[i].style.height = 'auto';
     }
-
-    if (window.innerWidth < 400) {
-        document.getElementById('pageTop').style.flexDirection = 'column';
-        document.getElementById('tyson').style.scale = '0.5';
-    }
+    size (841)
 }
