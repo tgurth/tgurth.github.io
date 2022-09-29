1f835305f3c7525ae34e6a7366a603813ddc846b
Tyler Gurth <tyler_gurth@brown.edu>
2022-09-29 14:57:59 -0400

    fixed dynamic sizing

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-window.addEventListener( 'resize', sizePictures );
+window.addEventListener( 'resize', sizePictures);
 
 size(600)
-
+let projectPictures = document.getElementsByClassName("projectPic");
 function size(min) {
     if (window.innerWidth < min) {
         document.getElementById('pageTop').style.flexDirection = 'column';
-        document.getElementById('tyson').style.scale = '0.5';
+        document.getElementById('tyson').style.scale = '0.8';
+        let projects = document.getElementsByClassName('projectBox')
+        for (let i = 0; i < projects.length; i++) {
+            projects[i].style.flexDirection = "column"
+        }
+
+        sizePictures(projectPictures)
+    } else {
+        document.getElementById('pageTop').style.flexDirection = 'row';
+        let projects = document.getElementsByClassName('projectBox')
+        for (let i = 0; i < projects.length; i++) {
+            projects[i].style.flexDirection = "row"
+        }
     }
 }
 
-
-/**
- * Sizing and resizing the images that hold the social medias.
- */
-let mediaPictures = document.getElementsByClassName('mediaPic');
-let projectPictures = document.getElementsByClassName('projectPic');
-sizePictures();
-
-function sizePictures() {
+sizePictures(projectPictures);
+function sizePictures(projectPictures) {
     for (let i = 0; i < projectPictures.length; i++) {
         let w = (window.innerWidth / 5) + 'px';
         projectPictures[i].style.width = w;    
         projectPictures[i].style.height = 'auto';
     }
-    size(841)
+    size(850);
 }
