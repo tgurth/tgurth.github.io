b6ebe843de2d3dc1df1c1900aede7657a9380408
Tyler Gurth <tyler_gurth@brown.edu>
2022-09-29 15:01:23 -0400

    expanded picture

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let projectPictures = document.getElementsByClassName("projectPic");
 function size(min) {
     if (window.innerWidth < min) {
         document.getElementById('pageTop').style.flexDirection = 'column';
-        document.getElementById('tyson').style.scale = '0.8';
+        document.getElementById('tyson').style.scale = '0.9';
         let projects = document.getElementsByClassName('projectBox')
         for (let i = 0; i < projects.length; i++) {
             projects[i].style.flexDirection = "column"
         }
 
-        sizePictures(projectPictures)
+        sizePictures(projectPictures, true)
     } else {
         document.getElementById('pageTop').style.flexDirection = 'row';
         let projects = document.getElementsByClassName('projectBox')
@@ -21,12 +21,14 @@ function size(min) {
     }
 }
 
-sizePictures(projectPictures);
+sizePictures(projectPictures, bool);
 function sizePictures(projectPictures) {
     for (let i = 0; i < projectPictures.length; i++) {
         let w = (window.innerWidth / 5) + 'px';
         projectPictures[i].style.width = w;    
         projectPictures[i].style.height = 'auto';
     }
-    size(850);
+    if (!bool) {
+        size(850);
+    }
 }
