9a84b9aa81b0682cc0f8746a2a361807874ed18c
Tyler Gurth <tyler_gurth@brown.edu>
2023-04-30 16:08:38 -0400

    added skills

--- main.js
+++ main.js
@@ -23,5 +23,18 @@ if (document.getElementById('about-opener') != null) {
   addSmoothScroll("about-anchor", "about-section");
   addSmoothScroll("project-anchor", "project-section");
   addSmoothScroll("experience-anchor", "experience-section");
+  addSmoothScroll("skill-anchor", "skill-section");
   addSmoothScroll("gallery-anchor", "gallery-section");
+}
+
+if (document.getElementById('about-opener') != null) {
+  let children = document.getElementsByClassName('skill-bubble');
+
+  for (let i = 0; i < children.length; i++) {
+    let size = (12 - i + 1)  / 2;
+    children[i].style.width = String(size) + 'em';
+    children[i].style.height = String(size) + 'em';
+
+    children[i].style.backgroundColor = "rgb(" + String(Math.random() * 255) + ', 255' + ',' + String(Math.random() * 255) +')';
+  }
 }
