2e66c1bb67118a5edbd04b2a202328941438d800
Tyler Gurth <tyler_gurth@brown.edu>
2023-05-31 21:00:32 -0400

    reverted to better site

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
 function addSmoothScroll(id, targetID) {
-    let anchor = document.getElementById(id);
+  let anchor = document.getElementById(id);
 
-    anchor.addEventListener('click', () => {        
-        let el = document.getElementById(targetID)
-        el.scrollIntoView({behavior: "smooth"});
-    });
+  anchor.addEventListener('click', () => {        
+      let el = document.getElementById(targetID)
+      el.scrollIntoView({behavior: "smooth"});
+  });
 }
 
-particlesJS.load('particles-js', 'particles/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
 
-if (document.getElementById('about-opener') != null) {
-  addSmoothScroll("about-anchor", "about-section");
-  addSmoothScroll("project-anchor", "project-section");
-  addSmoothScroll("experience-anchor", "experience-section");
-  addSmoothScroll("gallery-anchor", "gallery-section");
-}
-
-if (document.getElementById('about-opener') != null) {
-  let children = document.getElementsByClassName('skill-bubble');
-
-  for (let i = 0; i < children.length; i++) {
-    let size = (12 - i + 1)  / 2;
-    children[i].style.width = String(size) + 'em';
-    children[i].style.height = String(size) + 'em';
-  }
+if (document.getElementById('about-overlay') != null) {
+addSmoothScroll("about-anchor", "fake-nav");
+addSmoothScroll("project-anchor", "projects-title");
+addSmoothScroll("experience-anchor", "experience-title");
+addSmoothScroll("gallery-anchor", "gallery-title");
 }
